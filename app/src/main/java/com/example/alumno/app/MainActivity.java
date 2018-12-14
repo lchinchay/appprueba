2963374c548934606ee344abe19f67572d1165e2
Lucas Chinchay <chinchay.lucas@gmail.com>
2018-12-13 20:13:14 -0500

    login and app

---
+++ app/src/main/java/com/example/alumno/app/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.alumno.app;
+
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void siguiente(View view) {
+        Intent pasar = new Intent(this, inicio.class);
+        startActivity(pasar);
+    }
+}
